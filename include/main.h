186928c2476a3e857199320c3bddeae9138bcfd5
Jay Goyal <goyal.jay2003@gmail.com>
2024-10-28 14:57:10 +0530

    Create makefile and directory structure

---
+++ include/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
+#define MAX_COMMAND_SIZE 256
+
+#endif // !MAIN_H
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <main.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    char cmd[MAX_COMMAND_SIZE];
+    return EXIT_SUCCESS;
+}
